a523116fb7a12a771d984f5ef90a69f17b9aee58
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-12-02 19:47:43 +0200

    test_beap: Add tests for beap invariants checking.

--- test_beap.py
+++ test_beap.py
@@ -1,5 +1,5 @@
 # Run with nosetests3
-from beap import Beap
+from beap import Beap, VerifiedBeap
 
 
 def test_span_indexes():
@@ -12,3 +12,38 @@ def test_span_indexes():
     for i in range(100):
         start, end = Beap.span(i)
         assert end - start + 1 == i + 1
+
+
+def test_invariants():
+    # Data from Ian Munro's "ImpSODA06.ppt" presentation, Slide 3,
+    # with mistake corrected (21 and 22 not in beap order).
+    beap_data = [
+    72,
+    68, 63,
+    44, 62, 55,
+    33, 22, 32, 51,
+    13, 18, 21, 19, 22,
+    11, 12, 14, 17, 9, 13,
+     3,  2, 10
+    ]
+    beap = VerifiedBeap()
+    beap.arr = beap_data.copy()
+    beap.height = 6
+    beap.check_invariants()
+
+    beap.arr[0], beap.arr[1] = beap.arr[1], beap.arr[0]
+    try:
+        beap.check_invariants()
+        failed = True
+    except AssertionError:
+        failed = False
+    assert not failed, "Invariant checks don't work"
+
+    beap.arr = beap_data.copy()
+    beap.arr[0], beap.arr[len(beap.arr) - 1] = beap.arr[len(beap.arr) - 1], beap.arr[0]
+    try:
+        beap.check_invariants()
+        failed = True
+    except AssertionError:
+        failed = False
+    assert not failed, "Invariant checks don't work"
