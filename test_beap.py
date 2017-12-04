dd9c32be71ea22bbd8a504a6db15587184a8e17a
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-12-04 10:28:05 +0200

    test_beap: Add tests for search.

--- test_beap.py
+++ test_beap.py
@@ -2,6 +2,18 @@
 from beap import Beap, VerifiedBeap
 
 
+# Data from Ian Munro's "ImpSODA06.ppt" presentation, Slide 3,
+# with mistake corrected (21 and 22 not in beap order).
+BEAP_DATA = [
+    72,
+    68, 63,
+    44, 62, 55,
+    33, 22, 32, 51,
+    13, 18, 21, 19, 22,
+    11, 12, 14, 17, 9, 13,
+     3,  2, 10
+]
+
 def test_span_indexes():
     assert Beap.span(0) == (0, 0)
     assert Beap.span(1) == (1, 2)
@@ -15,19 +27,8 @@ def test_span_indexes():
 
 
 def test_invariants():
-    # Data from Ian Munro's "ImpSODA06.ppt" presentation, Slide 3,
-    # with mistake corrected (21 and 22 not in beap order).
-    beap_data = [
-    72,
-    68, 63,
-    44, 62, 55,
-    33, 22, 32, 51,
-    13, 18, 21, 19, 22,
-    11, 12, 14, 17, 9, 13,
-     3,  2, 10
-    ]
     beap = VerifiedBeap()
-    beap.arr = beap_data.copy()
+    beap.arr = BEAP_DATA.copy()
     beap.height = 6
     beap.check_invariants()
 
@@ -39,7 +40,7 @@ def test_invariants():
         failed = False
     assert not failed, "Invariant checks don't work"
 
-    beap.arr = beap_data.copy()
+    beap.arr = BEAP_DATA.copy()
     beap.arr[0], beap.arr[len(beap.arr) - 1] = beap.arr[len(beap.arr) - 1], beap.arr[0]
     try:
         beap.check_invariants()
@@ -47,3 +48,27 @@ def test_invariants():
     except AssertionError:
         failed = False
     assert not failed, "Invariant checks don't work"
+
+
+def test_search_once_off():
+    beap = VerifiedBeap()
+    beap.arr = BEAP_DATA.copy()
+    beap.height = 6
+
+    assert beap.search(51) == (9, 3)
+    assert beap.search(53) == None
+
+
+def test_search():
+    beap = VerifiedBeap()
+    beap.arr = BEAP_DATA.copy()
+    beap.height = 6
+
+    for i in range(101):
+        res = beap.search(i)
+        if i not in BEAP_DATA:
+            assert res is None
+        else:
+            idx, height = res
+            assert beap.arr[idx] == i
+            assert height <= beap.height
